5b750864d6b8bbab82868de85ff2f8b4aaf37913
sachin royal M <m.sachinroyal@gmail.com>
2025-07-21 22:49:55 +0530

    Update inverse pyramid.c
    added code

--- inverse pyramid.c
+++ inverse pyramid.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() 
+{
+    int n;
+    printf("enter the no:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<2*n-i-1;j++)
+        {
+            if(j<i)
+            {
+                printf("  ");
+            }
+            else
+            {
+                printf("* ");
+                
+            }
+        }
+            printf("\n");
+        
+    }
+    return 0;
+}
