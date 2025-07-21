7ef6c0cfedcbc287fbc35a02d2507ff5bfdc4423
sachin royal M <m.sachinroyal@gmail.com>
2025-07-21 22:41:49 +0530

    Create pramidpattern.c
    just created new file for pyramid pattern using c language

---
+++ pramidpattern.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() 
+{
+    int n;
+    printf("enter the no:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n+i;j++)
+        {
+            if(j<n-i-1)
+            {
+                printf("  ");
+            }
+            else
+            {
+                printf("* ");
+                
+            }
+        }
+            printf("\n");
+        
+    }
+    return 0;
+}
