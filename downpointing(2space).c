2092d0f980d6ad1c9c1da0a8b07d354811d49bc6
sachin royal M <m.sachinroyal@gmail.com>
2025-07-24 23:05:43 +0530

    Create downpointing(2space).c
    created down pointing pattern in c with 2 space

---
+++ downpointing(2space).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i ,j,n;
+    printf("enter the no :");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++){
+        for(j=0;j<=2*n+1;j++){
+            if((i<j) && (j<2*n+1-i)){
+                printf("  ");
+            }
+            else{
+                printf("* ");
+            }
+        }
+        printf("\n"); 
+    }
+    return 0;
+}
